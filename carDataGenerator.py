bugfix: Vinj formula was incorrect
Vinj formula at estimator is now equal to generator. Optimization is healthy now.

carDataGenerator.py
@@ -13,7 +13,7 @@ def changing_rpm(start_rpm, end_rpm, duration):
 # RPM values and intervals
 total_time = 600  # Total seconds
 intervals = [60, 120, 90, 150, 80, 100] 
-rpm_values = [1000, 2000, 1500, 2500, 1800, 2200] 
+rpm_values = [1000, 2000, 1500, 2500, 1800, 1000] 
 
 # RPM variation
 rpm_data = np.array([])

injectorEstimator.py
@@ -10,7 +10,7 @@ rho = 850
 def calculate_Vinj_cum(Cd, Pint, df):
 
     Pdif = df['Prail'] - Pint
-    Vinj = Cd * A * np.sqrt(2 * Pdif*100 / rho) * df['Tinj']/1000000
+    Vinj = Cd * A * np.sqrt(2 * Pdif*100000 / rho) * df['Tinj']/1000000
     df['Vinj cum'] = Vinj.cumsum()
     return df['Vinj cum'].iloc[-1]
 
@@ -89,17 +89,19 @@ def carDataGen_optimized(Cd,Pint,df):
     axs[1].grid(True)
 
     axs[1].set_title("Estimated")
-    axs[1].plot(df['RPM'])
-    axs[1].plot(df['Tinj'])
-    axs[1].plot(df['Prail'])
-    axs[1].plot(df['Vinj_cum'])
+    axs[1].plot(df['RPM'],label="RPM")
+    axs[1].plot(df['Tinj'],label="Tinj")
+    axs[1].plot(df['Prail'],label="Prail")
+    axs[1].plot(df['Vinj_cum'],label="Vinj_cum")
+    axs[1].legend()
 
     df2 = pd.read_csv("carData_1.csv")    
     axs[0].set_title("Experimental data (.csv)")
-    axs[0].plot(df2['RPM'])
-    axs[0].plot(df2['Tinj'])
-    axs[0].plot(df2['Prail'])
-    axs[0].plot(df2['Vinj_cum'])
+    axs[0].plot(df2['RPM'],label="RPM")
+    axs[0].plot(df2['Tinj'],label="Tinj")
+    axs[0].plot(df2['Prail'],label="Prail")
+    axs[0].plot(df2['Vinj_cum'],label="Vinj_cum")
+    axs[0].legend()
 
     plt.tight_layout()
     plt.show()